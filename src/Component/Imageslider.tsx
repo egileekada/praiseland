2c8e766acff6b489aa836df5b4cf5cab447eb9ff
egileekada <egileoniso.ekada@gmail.com>
2020-10-20 14:58:53 +0100

    Initial Commit

--- src/Component/Imageslider.tsx
+++ src/Component/Imageslider.tsx
@@ -88,7 +88,7 @@ export default class imageslider extends React.Component<any, any, any> {
                             <Row xs={12} md={12} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                             <Col  xs={9} md={9} lg={7} id='slide' >
                                 <h1 style={{textShadow:'3px 3px #0700008a'}} className="font-bold font-serif text-white items-center text-lg lg:text-4xl xl:text-4xl "  >{this.arr[this.state.COUNT].header}</h1>
-                                <p style={{textShadow:'3px 3px #0700008a'}} className="text-white font-mont-medium text-xs">{this.arr[this.state.COUNT].body}</p>      
+                                {/* <p style={{textShadow:'3px 3px #0700008a'}} className="text-white font-mont-medium text-xs">{this.arr[this.state.COUNT].body}</p>       */}
                             </Col>
                             <Col xs={7} md={7} lg={7} id='indicator' >
                                 <div className="flex carousel-indicators " style={{ display:'flex', justifyContent:'center', alignItems:'center' }}> 
