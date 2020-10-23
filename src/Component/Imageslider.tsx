74ada8b3c0656caf9dfa4439c6a17f24b7998264
egileekada <egileoniso.ekada@gmail.com>
2020-10-23 15:05:31 +0100

    Initial Commit

--- src/Component/Imageslider.tsx
+++ src/Component/Imageslider.tsx
@@ -6,6 +6,7 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
+import Modal from 'react-bootstrap/Modal'
 
 
 type State = {
@@ -89,7 +90,7 @@ export default class imageslider extends React.Component<any, any, any> {
                             <Col  xs={9} md={9} lg={7} id='slide' >
                                 <h1 style={{textShadow:'3px 3px #0700008a'}} className="font-bold font-serif text-white items-center text-lg lg:text-4xl xl:text-4xl "  >{this.arr[this.state.COUNT].header}</h1>
                                 {/* <p style={{textShadow:'3px 3px #0700008a'}} className="text-white font-mont-medium text-xs">{this.arr[this.state.COUNT].body}</p>       */}
-                                <Button className='font-bold' style={{position: 'absolute', bottom: '10px', height: '40px',width: '90px', fontSize:'16px' }} variant="success">Contact Us</Button>
+                                <Button className='font-bold' style={{position: 'absolute', bottom: '10px', height: '40px',width: '120px', fontSize:'16px' }} variant="success">Contact Us</Button>
                             </Col>
                         
                             <Col xs={7} md={7} lg={7} id='indicator' >
@@ -110,5 +111,26 @@ export default class imageslider extends React.Component<any, any, any> {
         </div>
     )
     }
+}
+
+function ModalBlock() {
+    const [show, setShow] = React.useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+        <Modal show={show} onHide={handleClose} centered >
+            <Modal.Header closeButton>
+              <Modal.Title className='font-bold font-serif'>
+                Online Giving
+              </Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              {/* <Dialog /> */}
+            </Modal.Body>
+            <Modal.Footer>
+              <Button onClick={handleClose} variant="secondary">Close</Button>                              
+            </Modal.Footer>
+          </Modal>
 }
 // className="flex h-16 carousel-indicators sm:bottom-0 "
