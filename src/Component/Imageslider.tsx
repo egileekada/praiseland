ab6a5d4bb42886fc358f25e5d14b2eabd136e78b
egileekada <egileoniso.ekada@gmail.com>
2020-10-29 12:10:27 +0100

    Initial Commit

--- src/Component/Imageslider.tsx
+++ src/Component/Imageslider.tsx
@@ -6,8 +6,9 @@ import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
-import {IoMdMail} from "react-icons/io"
-import Modal from 'react-bootstrap/Modal'
+// import {IoMdMail} from "react-icons/io"
+// import Modal from 'react-bootstrap/Modal'
+// import Mailto from 'reac'
 
 
 type State = {
@@ -90,8 +91,7 @@ export default class imageslider extends React.Component<any, any, any> {
                             <Row xs={12} md={12} style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
                             <Col  xs={9} md={9} lg={7} id='slide' >
                                 <h1 style={{textShadow:'3px 3px #0700008a'}} className="font-bold font-Poppinsbold text-white items-center text-lg lg:text-4xl xl:text-4xl "  >{this.arr[this.state.COUNT].header}</h1>
-                                {/* <p style={{textShadow:'3px 3px #0700008a'}} className="text-white font-mont-medium text-xs">{this.arr[this.state.COUNT].body}</p>       */}
-                                <Buttonblock />
+                                <Button className='font-bold font-Poppinsbold' href = "mailto: abc@example.com" style={{position: 'absolute', bottom: '10px' }} variant="success">Contact Us</Button>
                             </Col>
                         
                             <Col xs={7} md={7} lg={7} id='indicator' >
@@ -114,38 +114,51 @@ export default class imageslider extends React.Component<any, any, any> {
     }
 }
 
-function Buttonblock(){
-    const [show, setShow] = React.useState(false);
+// function Buttonblock(){
+//     const [show, setShow] = React.useState(false);
 
-    const handleClose = () => setShow(false);
-    const handleShow = () => setShow(true);
-    return(
-        <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
-        <Button className='font-bold font-Poppinsbold' onClick={handleShow} style={{position: 'absolute', bottom: '10px' }} variant="success">Contact Us</Button>
-            <Modal show={show} onHide={handleClose} centered >                
-                <Modal.Header style={{display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems: 'center' }} closeButton>
-                <div style={{flex:'1'}} />
-                <Modal.Title className='font-bold font-serif'>
-                    <img
-                        alt=""
-                        src= {require('../Image/logo.png')}
-                        width="50px"
-                        height="50px" 
-                        style={{marginLeft: '30px', marginRight: '10px', marginTop: '-5px'}}/>
-                </Modal.Title>
-                <Modal.Title className='font-Poppinsbold' >
-                    Contact Us
-                </Modal.Title>
-                <div style={{flex:'1'}} />
-                </Modal.Header>
-                <Modal.Body style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
-                    <h6 className='font-Poppins' >Click on Icon to contact the church: </h6>
-                    <IoMdMail size='50px' style={{marginTop:'5px', cursor: 'pointer'}} />
-                </Modal.Body>
-                <Modal.Footer>
-                <Button className='font-Poppinsbold' onClick={handleClose} variant="secondary">Close</Button>                              
-                </Modal.Footer>
-            </Modal>
-        </div>
-    )
-}
+//     // const email = 'egileoniso.ekada@gmail.com'
+
+//     const handleClose = () => setShow(false);
+//     const handleShow = () => setShow(true);
+//     return(
+//         <div style={{display:'flex', justifyContent:'center', alignItems:'center'}}>
+        
+//             <Modal show={show} onHide={handleClose} centered >                
+//                 <Modal.Header style={{display: 'flex', flexDirection: 'row', justifyContent:'center', alignItems: 'center' }} closeButton>
+//                 <div style={{flex:'1'}} />
+//                 <Modal.Title className='font-bold font-serif'>
+//                     <img
+//                         alt=""
+//                         src= {require('../Image/logo.png')}
+//                         width="50px"
+//                         height="50px" 
+//                         style={{marginLeft: '30px', marginRight: '10px', marginTop: '-5px'}}/>
+//                 </Modal.Title>
+//                 <Modal.Title className='font-Poppinsbold' >
+//                     Contact Us
+//                 </Modal.Title>
+//                 <div style={{flex:'1'}} />
+//                 </Modal.Header>
+//                 <Modal.Body style={{display:'flex', flexDirection:'column', alignItems:'center'}}>
+//                     <h6 className='font-Poppins' >Click on Icon to contact the church: </h6>
+//                     <IoMdMail size='50px' style={{marginTop:'5px', cursor: 'pointer'}} >
+//                         {/* <Mailto email="egileoniso.ekada@gmail.com" subject="Hello & Welcome" body="Hello world!" /> */}
+//                         {/* <a href={`mailto: egileoniso.ekada@gmail.com`}/> */}
+//                         {/* <Mailto email="egileoniso.ekada@gmail.com" obfuscate={true}/> */}
+//                     </IoMdMail>
+
+//                 </Modal.Body>
+//                 <Modal.Footer>
+//                 <Button className='font-Poppinsbold' onClick={handleClose} variant="secondary">Close</Button>                              
+//                 </Modal.Footer>
+//             </Modal>
+//         </div>
+//     )
+// }
+
+// const Mailto = ({ email, subject, body, children }: any) => {
+//     return (
+//       <a href={`mailto:${email}?subject=${encodeURIComponent(subject) || ''}&body=${encodeURIComponent(body) || ''}`}>{children}</a>
+//     );
+//   };
